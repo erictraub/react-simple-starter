dc79fe0871390cac8749eff0d7b363eba973f726
Eric Traub <eric@hedgeable.com>
2017-08-29 18:07:20 -0400

    work with state and callbacks

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyCH2rSyzmBV3oouvjFZF-tYv8QkX7vQS7E';
 
 
@@ -12,19 +13,27 @@ class App extends Component {
 		super(props);
 
 		this.state = {
-			videos: []
+			videos: [],
+			selectedVideo: null
 		};
 
 		YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
-			this.setState({ videos });
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
 		});
 	}
+
 
 	render () {
 		return (
 			<div>
 				<SearchBar />
-				<VideoList videos={this.state.videos} />
+				<VideoDetail video={this.state.selectedVideo}/>
+				<VideoList
+					onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+					videos={this.state.videos} />
 			</div>
 		);
 	}
